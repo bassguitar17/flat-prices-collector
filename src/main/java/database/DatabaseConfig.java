bf4356950c34f38627cdaa16a1dc295f4c75e0a4
Rumun <bassguitar17@gmail.com>
2021-01-10 00:48:28 +0100

    Revert "first commit"
    This reverts commit 17624ea7188d0d5eeae13135bef61fd76e0fba1c.

--- src/main/java/database/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package database;
-
-public class DatabaseConfig {
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/flats_test?useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static final String JDBC_ACCOUNT = "roman";
-    public static final String JDBC_PASSWORD = "123";
-}
--- src/main/java/starters/Main.java
+++
@@ -1,7 +0,0 @@
-package starters;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("siemaszko! Let's go! ");
-    }
-}
